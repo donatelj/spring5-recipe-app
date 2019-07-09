a0e4f73185b5254276a1d37949f1b93d95172246
Jean-Michel Donatelli <jean-michel.donatelli@desjardins.com>
2019-07-09 15:12:44 -0400

    Modified .gitignore file for CircleCI. (4)

--- src/test/java/guru/springframework/repositories/UnitOfMesureRepositoryIT.java
+++ src/test/java/guru/springframework/repositories/UnitOfMesureRepositoryIT.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
@@ -26,7 +25,6 @@ public class UnitOfMesureRepositoryIT {
     }
 
     @Test
-    @DirtiesContext
     public void findByDescription() throws Exception{
         Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMesureRepository.findByDescription("Teaspoon");
 
